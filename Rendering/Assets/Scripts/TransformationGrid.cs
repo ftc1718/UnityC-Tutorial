4bb10f2f96d9e978d92e54cfa2ee79f35331638a
Cherno <ftc1718@163.com>
2018-04-23 17:45:54 +0800

    Grid transform and rotate with z axis

--- Rendering/Assets/Scripts/TransformationGrid.cs
+++ Rendering/Assets/Scripts/TransformationGrid.cs
@@ -2,15 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransformationGrid : MonoBehaviour {
+public class TransformationGrid : MonoBehaviour 
+{
+    public Transform prefab;
+    public int gridResolution = 10;
+    Transform[] grid;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    List<Transformation> transformations;
+    // Transformation[] trans;
+
+    Transform CreateGridPoint(int x, int y, int z)
+	{
+        Transform point = Instantiate<Transform>(prefab);
+        point.localPosition = GetCoordinates(x, y, z);
+        point.GetComponent<MeshRenderer>().material.color = new Color(
+            (float)x / gridResolution,
+            (float)y / gridResolution,
+            (float)z / gridResolution
+        );
+        return point;
+    }
+
+	Vector3 GetCoordinates(int x, int y, int z)
+	{
+        return new Vector3(
+			x - (gridResolution - 1) * 0.5f,
+			y - (gridResolution - 1) * 0.5f,
+			z - (gridResolution - 1) * 0.5f
+		);
+    }
+
+	Vector3 TransformPoint(int x, int y, int z)
+	{
+        Vector3 coordinates = GetCoordinates(x, y, z);
+        for (int i = 0; i < transformations.Count; i++)
+		{
+            coordinates = transformations[i].Apply(coordinates);
+        }
+            return coordinates;
+    }
+
+	void Awake()
+	{
+		transformations = new List<Transformation>();
+
+        grid = new Transform[gridResolution * gridResolution * gridResolution];
+        for (int i = 0, z = 0; z < gridResolution; z++)
+		{
+            for (int y = 0; y < gridResolution; y++)
+			{
+                for (int x = 0; x < gridResolution; x++, i++)
+				{
+                    grid[i] = CreateGridPoint(x, y, z);
+                }
+            }
+        }
+    }
+
+	void Update()
+	{
+        GetComponents<Transformation>(transformations);
+        // trans = GetComponents<Transformation>();
+
+        for (int i = 0, z = 0; z < gridResolution; z++)
+		{
+            for (int y = 0; y < gridResolution; y++)
+			{
+                for (int x = 0; x < gridResolution; x++, i++)
+				{
+                    grid[i].localPosition = TransformPoint(x, y, z);
+                }
+
+            }
+        }
+    }
 }
