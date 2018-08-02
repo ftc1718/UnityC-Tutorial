6152d1662eb8c2201006ef1ba1fde8a0b8522d7e
Cherno <ftc1718@163.com>
2018-08-02 15:38:30 +0800

    Visualizing the CoC

---
+++ Advanced Rendering/Assets/Scripts/DepthOfFieldEffect.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class DepthOfFieldEffect : MonoBehaviour
+{
+    [HideInInspector]
+    public Shader dofShader;
+
+    [NonSerialized]
+    Material dofMaterial;
+
+    const int circleOfConfusionPass = 0;
+
+    [Range(0.1f, 100f)]
+    public float focusDistance = 10f;
+    [Range(0.1f, 10f)]
+    public float focusRange = 3f;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		if(dofMaterial == null)
+		{
+            dofMaterial = new Material(dofShader);
+            dofMaterial.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        dofMaterial.SetFloat("_FocusDistance", focusDistance);
+        dofMaterial.SetFloat("_FocusRange", focusRange);
+
+        // RenderTexture coc = RenderTexture.GetTemporary(
+        //     src.width, src.height, 0, RenderTextureFormat.RHalf, RenderTextureReadWrite.Linear
+        // );
+
+        Graphics.Blit(src, dest, dofMaterial, circleOfConfusionPass);
+        // Graphics.Blit(coc, dest);
+
+        // RenderTexture.ReleaseTemporary(coc);
+    }
+}
