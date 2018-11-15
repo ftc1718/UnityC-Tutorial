ad3804bf034f278eb974c30fb7070f304f560ff6
Cherno <ftc1718@163.com>
2018-11-15 14:19:46 +0800

    [Modify] change folder structure

---
+++ Scriptable Render Pipeline/Assets/My Pipeline/MyRP/Editor/MyShaderIncludePaths.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+static class MyShaderIncludePaths
+{
+	[ShaderIncludePath]
+    static string[] GetPaths()
+	{
+        return new string[]
+        {
+            Path.GetFullPath("Assets/My Pipeline")
+        };
+    }
+}
---
+++ Scriptable Render Pipeline/Assets/My Pipeline/MyRP/Shaders/Unlit.shader
@@ -0,0 +1,20 @@
+﻿Shader "My Pipeline/Unlit"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Pass
+		{
+			HLSLPROGRAM
+			#pragma target 3.5
+			
+			#pragma vertex UnlitPassVertex
+			#pragma fragment UnlitPassFragment
+
+			#include "MyRP/ShaderLibrary/Unlit.hlsl"
+			ENDHLSL
+		}
+	}
+}
