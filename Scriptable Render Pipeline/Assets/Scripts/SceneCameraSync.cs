b3e7f740817cafd902cbf888454d72a37051cd6e
Cherno <ftc1718@163.com>
2019-03-14 17:07:53 +0800

    [Modify] semi-transparent objects render

---
+++ Scriptable Render Pipeline/Assets/Scripts/SceneCameraSync.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SceneCameraSync : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnRenderObject()
+    {
+        if (Camera.current != null)
+        {
+            if (Camera.current.name.Equals("SceneCamera"))
+            {
+                transform.position = Camera.current.transform.position;
+                transform.rotation = Camera.current.transform.rotation;
+            }
+        }
+    }
+}
