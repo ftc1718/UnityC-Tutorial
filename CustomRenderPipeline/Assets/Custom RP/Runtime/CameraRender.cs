7f8901e3a3a11d11287377d8f270ca3d6c082295
Cherno <ftc1718@163.com>
2020-01-09 17:39:17 +0800

    [Update] drawing unlit object

---
+++ CustomRenderPipeline/Assets/Custom RP/Runtime/CameraRender.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRender
+{
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    ScriptableRenderContext context;
+    Camera camera;
+    CullingResults cullingResults;
+
+    const string bufferName = "Render Camera";
+
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        if(!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExcuteBuffer();
+    }
+
+    void DrawVisibleGeometry()
+    {
+        SortingSettings sortingSettings = new SortingSettings(camera) { criteria = SortingCriteria.CommonOpaque};
+        DrawingSettings drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+        context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExcuteBuffer();
+        context.Submit();
+    }
+
+    void ExcuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+}
---
+++ CustomRenderPipeline/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    CameraRender renderer = new CameraRender();
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach(Camera camera in cameras)
+        {
+
+            renderer.Render(context, camera);
+        }
+    }
+}
