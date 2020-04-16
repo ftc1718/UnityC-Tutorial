e6b38b38e7da9ee3f901423f5e677c60da68acd0
Cherno <ftc1718@163.com>
2020-04-16 11:31:42 +0800

    [Update] rendering direction light shadow atlas

--- CustomRenderPipeline/Assets/Custom RP/Runtime/Shadows.cs
+++ CustomRenderPipeline/Assets/Custom RP/Runtime/Shadows.cs
@@ -66,7 +66,30 @@ public class Shadows
         buffer.GetTemporaryRT(dirShadowAtlasId, atlasSize, atlasSize, 32, FilterMode.Bilinear, RenderTextureFormat.Shadowmap);
         buffer.SetRenderTarget(dirShadowAtlasId, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store);
         buffer.ClearRenderTarget(true, false, Color.clear);
+        buffer.BeginSample(bufferName);
         ExecuteBuffer();
+
+        for (int i = 0; i < ShadowedDirectionalLightCount; i++)
+        {
+            RenderDirectionalShadows(i, atlasSize);
+        }
+
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+    }
+
+    void RenderDirectionalShadows(int index, int tileSize)
+    {
+        ShadowedDirectionalLight light = ShadowedDirectionalLights[index];
+        var shadowSettings =
+            new ShadowDrawingSettings(cullingResults, light.visibleLightIndex);
+
+        cullingResults.ComputeDirectionalShadowMatricesAndCullingPrimitives(light.visibleLightIndex, 0, 1,
+            Vector3.zero, tileSize, 0, out Matrix4x4 viewMatrix, out Matrix4x4 projMatrix, out ShadowSplitData shadowSplitData);
+        shadowSettings.splitData = shadowSplitData;
+        buffer.SetViewProjectionMatrices(viewMatrix, projMatrix);
+        ExecuteBuffer();
+        context.DrawShadows(ref shadowSettings);
     }
 
     void ExecuteBuffer()
